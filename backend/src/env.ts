fb0ba3ee990641a9eacd77be689ab20279f63cad
Sergei Dorogin <segy@google.com>
2021-05-25 18:42:33 +0300

    Fix: BE: fix handling empty env variable (GAE returns 'None' for them and it breaks some things, e.g. fetching scheduler jobs)
    Change-Id: I2343ee8bab82e137ba7f10e3ad775d6c77c2c63e

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -16,30 +16,42 @@
 import path from 'path';
 import fs from 'fs';
 import argv from './argv';
-import { downloadFileFromGCS } from './app/cloud-storage';
-export const STATIC_DIR = argv.staticDir || process.env.STATIC_DIR || 'static';
-export const PORT = argv.port || process.env.PORT || 8080;
-export const HEALTH_CHECK_URL = argv.healthCheckUrl || process.env.HEALTH_CHECK_URL || '/_ah/health';
+
+/**
+ * App Engine for an environemnt variable in app.yaml with empty value return 'None',
+ * we'are converting 'None' to '' to simplify things. 
+ * @param envvar an environment variable's value
+ * @returns 
+ */
+function getVal(envvar: any) {
+  if (envvar === 0) return envvar;
+  if (!envvar || envvar === 'None') return '';
+  return envvar;
+}
+
+export const STATIC_DIR = argv.staticDir || getVal(process.env.STATIC_DIR) || 'static';
+export const PORT = argv.port || getVal(process.env.PORT) || 8080;
+export const HEALTH_CHECK_URL = argv.healthCheckUrl || getVal(process.env.HEALTH_CHECK_URL) || '/_ah/health';
 // Supported values: 'IAP' (Identity-Aware Proxy), 'CLIENT' (auth tokens in Authotization header):
-export const SECURITY = argv.security || process.env.SECURITY || 'CLIENT';
+export const SECURITY = argv.security || getVal(process.env.SECURITY) || 'CLIENT';
 
 // applicabale for SECURITY=='IAP', it's a value from 'Signed Header JWT Audience' 
 // on https://console.cloud.google.com/security/iap?project=YOUR_PROJECT
 // See https://cloud.google.com/iap/docs/signed-headers-howto#verifying_the_jwt_payload
-export const EXPECTED_AUDIENCE = argv.expectedAudience || process.env.EXPECTED_AUDIENCE;
+export const EXPECTED_AUDIENCE = argv.expectedAudience || getVal(process.env.EXPECTED_AUDIENCE);
 
 /** Google Spreadsheet identifier for master doc with links to configuration spreadsheets */
-export const MASTER_SPREADSHEET = argv.masterSpreadsheet || process.env.MASTER_SPREADSHEET;
+export const MASTER_SPREADSHEET = argv.masterSpreadsheet || getVal(process.env.MASTER_SPREADSHEET);
 
 /** True if the app is inside AppEngine */
 export const IS_GAE = !!process.env.GAE_APPLICATION;
 
-export const GAE_LOCATION = process.env.GAE_LOCATION || "europe-west1";
+export const GAE_LOCATION = getVal(process.env.GAE_LOCATION) || "europe-west1";
 
 /** Default log level (usualy one of 'info' or 'debug') */
-export const LOG_LEVEL = argv.logLevel || process.env.LOG_LEVEL || (process.env.NODE_ENV === "production" ? "info" : "debug");
+export const LOG_LEVEL = argv.logLevel || getVal(process.env.LOG_LEVEL) || (process.env.NODE_ENV === "production" ? "info" : "debug");
 
-const cloudLogging = (argv.cloudLogging || process.env.CLOUD_LOGGING || '').toLowerCase()
+const cloudLogging = (argv.cloudLogging || getVal(process.env.CLOUD_LOGGING) || '').toLowerCase()
 /** true to use Cloud Logging anyway (even when running locally), false - disable Cloud Logging, undefined/default - use default */
 export const CLOUD_LOGGING = cloudLogging === 'true' ? true : (cloudLogging === 'false' ? false : true);
 
@@ -54,9 +66,8 @@ export function getTempDir() {
 export const SERVICE_ACCOUNT = IS_GAE ? process.env.GOOGLE_CLOUD_PROJECT + '@appspot.gserviceaccount.com' : ''
 
 let mailer_config: any = undefined;
-const mailerConfigPath = argv.mailerConfigPath || process.env.MAILER_CONFIG_PATH;
+const mailerConfigPath = argv.mailerConfigPath || getVal(process.env.MAILER_CONFIG_PATH);
 if (mailerConfigPath) {
-  // TODO: suppport gs:// pathes
   let filepath = path.join(__dirname, '..', mailerConfigPath);
   filepath = path.resolve(filepath);
   if (fs.existsSync(filepath)) {
@@ -67,7 +78,22 @@ if (mailerConfigPath) {
     } catch(e) {
       console.error(`Failed to read nodemailer config in ${filepath}:\n`, e);
     }
+  } else {
+    console.warn(`File with nodemailer config '${filepath}' doesn't exist`);
   }
 }
 /** Configuration object for NodeMailer */
 export const MAILER_CONFIG = mailer_config;
+
+console.debug(`STATIC_DIR: '${STATIC_DIR}'
+PORT: '${PORT}'
+HEALTH_CHECK_URL: '${HEALTH_CHECK_URL}'
+SECURITY: '${SECURITY}'
+EXPECTED_AUDIENCE: '${EXPECTED_AUDIENCE}'
+MASTER_SPREADSHEET: '${MASTER_SPREADSHEET}'
+IS_GAE: '${IS_GAE}'
+GAE_LOCATION: '${GAE_LOCATION}'
+LOG_LEVEL: '${LOG_LEVEL}'
+CLOUD_LOGGING: '${CLOUD_LOGGING}'
+MAILER_CONFIG_PATH: '${mailerConfigPath}'
+`);
